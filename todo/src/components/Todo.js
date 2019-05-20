0db7493c688599011b7ec684e59d8779f82ef740
Sydney Crumley <srsimps19@gmail.com>
2019-05-20 16:50:51 -0500

    added provider to index.js  and reducer index

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 class Todo extends React.Component {
-    
+  state = {
+      todos: [
+          {
+              value: '',
+              completed: false
+          }
+      ]
+  }  
+
+  render() {
+
+  }
 }
