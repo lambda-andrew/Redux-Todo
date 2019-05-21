85ab51b7a4c0a69cb5a4c152a6535eda03b170ae
Sydney Crumley <srsimps19@gmail.com>
2019-05-21 18:00:13 -0500

    fixed the input to clear after submit

--- todo/src/components/TodoHome.js
+++ todo/src/components/TodoHome.js
@@ -14,6 +14,7 @@ class TodoHome extends React.Component {
   addTodo = e => {
     e.preventDefault()
    this.props.addTodo(this.state.newTodos)
+   this.setState({newTodos: ""})
   }
 
   toggleTodo = (e, id) => {
