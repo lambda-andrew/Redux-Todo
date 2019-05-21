ccfd8dcca1f26544b423f96137eb8497230665d5
Sydney Crumley <srsimps19@gmail.com>
2019-05-21 14:19:44 -0500

    added toggle case to reducer

--- todo/src/components/reducers/index.js
+++ todo/src/components/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_TODO } from '../actions'
+import { ADD_TODO, TOGGLE_TODO } from '../actions'
 
 const initialState = {
     todos: [
@@ -21,6 +21,22 @@ export const reducer = (state = initialState, action) => {
                     completed: false
                 })
             }
+        case TOGGLE_TODO:
+            return {
+                ...state,
+                todos: state.todos.map(todo => {
+                    if (todo.id === action.payload) {
+                        return {
+                            ...todo, 
+                            completed: !todo.completed
+                        } 
+                    }
+                    else {
+                        return todo;
+                    }
+                }
+                )
+            } 
         default:
             return state;
     }
